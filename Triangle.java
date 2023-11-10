b4aa517c935e514f66b98d784198bedd25f9d41d
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-11-10 10:47:54 +0700

    PERCOBAAN 3 DASPRO NO.2 PERTANYAAN

--- Triangle.java
+++ Triangle.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); System.out.print("Masukkan nilai N = "); int N = sc.nextInt();
-int i = 0; while(i <= N) {
-int j = 0; while(j < i) {
-System.out.print("*"); j++;
-} i++;
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Masukkan nilai N = "); 
+        int N = sc.nextInt();
+        int i = 1; //mengubah nilai i dari 0 jadi mulai dari 1
+        while(i <= N) {
+        int j = 0; 
+        while(j < i) {
+        System.out.print("*"); 
+        j++;
+} 
+System.out.println();
+i++;
 }
 
 
