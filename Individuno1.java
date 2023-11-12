b676fde1f98dbcb5600e1dbb573c2062c7e55788
Lyrafaiqahh <lyrafaiqah@gmail.com>
2023-11-12 07:33:49 +0700

    INDIVIDU TUGAS

--- Individuno1.java
+++ Individuno1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Individuno1 {
-    // TINGGAL BALIKIN AJA DARI 1  KE PANJANG
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
